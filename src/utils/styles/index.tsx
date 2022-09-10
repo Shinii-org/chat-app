cd27d18c6f6d7c3ff4d419d5ccc3adf8d3c30308
ngocphu1996 <ngocphu280996@gmail.com>
2022-09-10 12:51:01 +0700

    create register page

---
+++ src/utils/styles/index.tsx
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+
+export const InputField = styled.input`
+  width: 100%;
+  outline: none;
+  font-family: "Inter";
+  border: none;
+  color: #fff;
+  font-size: 18px;
+  box-sizing: border-box;
+  background-color: inherit;
+  padding: 0;
+  margin: 4px 0;
+`;
+
+export const InputContainer = styled.div`
+  background-color: #131313;
+  padding: 12px 16px;
+  border-radius: 10px;
+  width: 100%;
+  box-sizing: border-box;
+`;
+
+export const InputLabel = styled.label`
+  display: block;
+  color: #8f8f8f;
+  font-size: 14px;
+  margin: 4px 0;
+`;
+
+export const Button = styled.button`
+  width: 100%;
+  color: #fff;
+  outline: none;
+  border: none;
+  font-family: "Inter";
+  font-size: 16px;
+  border-radius: 10px;
+  background-color: #2b09ff;
+  padding: 25px 0;
+`;
+
+export const Page = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  background-color: #1a1a1a;
+  align-items: center;
+`;
